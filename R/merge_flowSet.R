a37c45b5a49a052de6efd4102c07787ba663bda4
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2017-07-12 11:36:06 -0500

    added smart column renaming

--- R/merge_flowSet.R
+++ R/merge_flowSet.R
@@ -6,9 +6,12 @@
 #' @return a tidy dataframe of flow cytometry data; rows are observations, columns are channels; the identifier column is by default the name of the fcs file and is called 'exp'
 
 merge_flowSet <- function(fs, rename.cols = T){
-  plyr::rename(plyr::ldply(fsApply(fs, function(x) as.data.frame(exprs(x)), simplify = FALSE)), c(".id" = "exp")) %>%
+  df <- plyr::rename(plyr::ldply(fsApply(fs, function(x) as.data.frame(exprs(x)), simplify = FALSE)), c(".id" = "exp")) %>%
     select(-exp, everything())
-  #if (rename.cols == T) {
-  #  rename_fcs_cols(fs)
-  #}
+  if (rename.cols == T) {
+    df <- rename_fcs_cols(df)
+    return(df)
+  } else {
+    return(df)
+  }
 }
--- R/rename_fcs_cols.R
+++ R/rename_fcs_cols.R
@@ -1,9 +1,9 @@
 #' Rename the columns of a dataframe intelligently
-#' 
+#'
 #' @param dataframe the name of the dataframe to be renamed
 #' @param print.names whether or not to print the resulting column names; default is F
 #' @export
-#' @return a dataframe in the same format as merge_flowSet, but with neat column names
+#' @return a dataframe in the same format as merge_flowSet, but with shorter column names
 
 rename_fcs_cols <- function(dataframe, print.names = F){
   default.column.names <- colnames(dataframe)[1:(ncol(dataframe)-2)]
@@ -12,4 +12,5 @@ rename_fcs_cols <- function(dataframe, print.names = F){
   if (print.names == TRUE){
     print(regmatches(default.column.names, m))
   }
+  return(dataframe)
 }
