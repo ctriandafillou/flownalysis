d5018ec771dc29c98c15ad94ef68eb196839fea7
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2017-08-04 16:24:26 -0500

    updated cc.analysis help document

--- R/cc.analysis.R
+++ R/cc.analysis.R
@@ -3,11 +3,13 @@
 #' This function takes raw data from a calibration curve experiment (yeast expressing pHluorin, flow cytometry data) and does all processing steps to construct a calibration curve that maps fluorescence ratio to pH. Takes *raw* (untransformed) FITC and BV510 channels as inputs!
 #' @param subset The dataframe containing the raw calibration curve data. Identifier column should be called 'exp' and formatted as 'cc_pH.fcs'
 #' @param background The dataframe containing the raw background data. Identifier column should be called 'exp' and formatted as 'experiment_strain_background' with background being either 'buffer' or 'media'
-#' @param id Name/date of experiment
-#' @param buffer.values A list of buffer values for that day
-#' @param inputs A list of column entries that were used during data collection
+#' @param id Name/date of experiment, will end up as part of the cc function name so use plaintext characters and no dashes
+#' @param buffer.values A list of buffer values for that day, default is FALSE (inferred from filenames)
+#' @param inputs A list of column entries that were used during data collection, default is FALSE (inferred from filenames)
 #' @param xmin minimum pH predicted (depends on range of buffer values used)
 #' @param xmax maximum pH predicted (depends on range of buffer values used)
+#' @param FITC.thresh Value that untransformed FITC.A (area) signal must exceed; default is 800
+#' @param BV.thresh Value that untransformed BV510.A (area) signal must exceed; default is 800
 #' @export
 #' @return convert.to.pH function, plot of calibration curve, analysis of fit quality
 
