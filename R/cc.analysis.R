0c87e79ed0ba95a3164fdf21207768573890fd28
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2017-08-11 14:38:35 -0500

    updated cc analysis to take starting fitting parameters as arguments

--- R/cc.analysis.R
+++ R/cc.analysis.R
@@ -10,12 +10,13 @@
 #' @param xmax maximum pH predicted (depends on range of buffer values used)
 #' @param FITC.thresh Value that untransformed FITC.A (area) signal must exceed; default is 800
 #' @param BV.thresh Value that untransformed BV510.A (area) signal must exceed; default is 800
+#' @param start.list Edit the starting fitting parameters; default is list(a=.5, b=2, c=7, d=0.25) (keep this form but change numbers)
 #' @export
 #' @return convert.to.pH function, plot of calibration curve, analysis of fit quality
 
 
 
-cc.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALSE, xmin=4.9, xmax=8.6, FITC.thresh = 800, BV.thresh = 800){
+cc.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALSE, xmin=4.9, xmax=8.6, FITC.thresh = 800, BV.thresh = 800, start.list = list(a=.5, b=2, c=7, d=0.25)){
   bkgs <- background %>%
     separate(exp, c("experiment", "strain", "background"), convert=T, extra="drop") %>%
     group_by(strain, background, experiment) %>%
@@ -26,46 +27,48 @@ cc.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALS
   
   FITC.m.bkg <- as.numeric(filter(bkgs, strain == "by4743" & background == "media")$FITC.A)
   BV.m.bkg <- as.numeric(filter(bkgs, strain == "by4743" & background == "media")$BV510.A)
-
+  
   if (inputs == FALSE){
     cc <- subset %>%
       filter(FITC.A > FITC.thresh & BV510.A > BV.thresh) %>% # High-quality points only
       separate(exp, c("experiment", "pH"), extra="drop") %>%
       mutate(pH = as.numeric(gsub("p", ".", pH)),
-             pH.ratio = (BV510.A - BV.bkg) / (FITC.A - FITC.bkg)) 
+             pH.ratio = (BV510.A - BV.bkg) / (FITC.A - FITC.bkg))
   } else{
     cc <- subset %>%
       filter(FITC.A > FITC.thresh & BV510.A > BV.thresh) %>% # High-quality points only
       separate(exp, c("experiment", "pH"), extra="drop") %>%
       mutate(pH = as.numeric(plyr::mapvalues(pH, from=inputs, to=buffer.values)), pH.ratio = (BV510.A - BV.bkg) / (FITC.A - FITC.bkg))
   }
-  
+
   cc.quality <- nrow(cc)/nrow(subset)*100
   
+
   cc <- cc %>%
     group_by(pH) %>%
     summarise(med.ratio = median(pH.ratio), high = quantile(pH.ratio, 0.75), low = quantile(pH.ratio, 0.25))
-  
+ 
+
   sigmoid <- function(params, x) {
     (params[1] / (1 + exp(-params[2] * (x-params[3])))) + params[4]
   }
-  
+
   x <- cc$pH
   y <- cc$med.ratio
-  
-  fitmodel <- nls(y~a/(1 + exp(-b * (x-c))) + d, start=list(a=.5, b=2, c=7, d=0.25))
+
+  fitmodel <- nls(y~a/(1 + exp(-b * (x-c))) + d, start=start.list)
   params = coef(fitmodel)
-  
+
   x2 <- seq(xmin, xmax, 0.01)
   y2 <- sigmoid(params, x2)
-  
-  q <- ggplot(data=NULL) + geom_point(aes(x=x, y=y)) + geom_line(aes(x=x2, y=y2), size=0.5) 
+
+  q <- ggplot(data=NULL) + geom_point(aes(x=x, y=y)) + geom_line(aes(x=x2, y=y2), size=0.5)
   q <- q+ geom_errorbar(data=cc, aes(x=pH, ymin=low, ymax=high), size=0.5, width=0.05)
   q <- q + theme_minimal() + labs(
     x = "pH",
     y = "Ratio 405/488",
     title = paste("pHluorin calibration curve", id, sep = " ")) + geom_text(aes(x=5.8, y=0.55, label="ratio==frac(a,1+e^(-b(x-c))) + d"), parse = T)
-  
+
   convert.to.pH <- function(FITC.value=NA, BV.value=NA, ratio.value=NA, p=params, lims=y2, ratio=F){
     # ratio = [a / (1 + exp(-b(pH-c)))] + d
     # pH = [log(a / (ratio - d) - 1) / -b] + c
@@ -77,7 +80,7 @@ cc.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALS
   }
   fxn.name <- paste("convert.to.pH", as.character(id), sep=".")
   assign(fxn.name, convert.to.pH, envir = .GlobalEnv)
-  
+
   # Completetion statement:
   cat("Calibration curve with experiment ID \'", id, "\' complete:\n", sep="")
   print(q)
