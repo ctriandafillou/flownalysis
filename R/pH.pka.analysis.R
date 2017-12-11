4d758eacaabb6e0604e16d0d6b06125794c9da9e
Cat Triandafillou <ctriandafillou@uchicago.edu>
2017-12-11 08:36:26 -0600

    added pka analysis fxn

--- R/pH.pka.analysis.R
+++ R/pH.pka.analysis.R
@@ -11,12 +11,13 @@
 #' @param FITC.thresh Value that untransformed FITC.A (area) signal must exceed; default is 800
 #' @param BV.thresh Value that untransformed BV510.A (area) signal must exceed; default is 800
 #' @param start.list Edit the starting fitting parameters; default is list(a=.5, b=2, c=7, d=0.25) (keep this form but change numbers)
+#' @param return possible values: "plot" for full analysis, "value" for the value only
 #' @export
 #' @return convert.to.pH function, plot of calibration curve, analysis of fit quality
 
 
 
-cc.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALSE, xmin=4.9, xmax=8.6, FITC.thresh = 800, BV.thresh = 800, start.list = list(a=.5, b=2, c=7, d=0.25)){
+pH.pka.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALSE, xmin=4.9, xmax=8.6, FITC.thresh = 800, BV.thresh = 800, start.list = list(a=.5, b=2, c=7, d=0.25), return = "plot"){
   bkgs <- background %>%
     separate(exp, c("experiment", "strain", "background"), convert=T, extra="drop") %>%
     group_by(strain, background, experiment) %>%
@@ -56,34 +57,23 @@ cc.analysis <- function(subset, background, id, inputs=FALSE, buffer.values=FALS
   x <- cc$pH
   y <- cc$med.ratio
 
-  fitmodel <- nls(y~a/(1 + exp(-b * (x-c))) + d, start=start.list)
-  params = coef(fitmodel)
+  pka <- data.frame(x,y) %>%
+    mutate(log.ratio = -log10((y-max(y))/(min(y)-y))) %>%
+    filter(!is.na(log.ratio) & !is.infinite(log.ratio))
+  colnames(pka) <- c("pH", "med.ratio", "log.ratio")
+  pka.fit <- lm(pka$log.ratio~pka$pH)
+  pka.model <- coef(pka.fit)
 
-  x2 <- seq(xmin, xmax, 0.01)
-  y2 <- sigmoid(params, x2)
+  if (return == "value") {
+    return(-pka.model[[1]]/pka.model[[2]])
+  } else {
 
-  q <- ggplot(data=NULL) + geom_point(aes(x=x, y=y)) + geom_line(aes(x=x2, y=y2), size=0.5)
-  q <- q+ geom_errorbar(data=cc, aes(x=pH, ymin=low, ymax=high), size=0.5, width=0.05)
-  q <- q + theme_minimal() + labs(
-    x = "pH",
-    y = "Ratio 405/488",
-    title = paste("pHluorin calibration curve", id, sep = " ")) + geom_text(aes(x=5.8, y=0.55, label="ratio==frac(a,1+e^(-b(x-c))) + d"), parse = T)
+  plot(pka$pH, pka$log.ratio, xlab="pH", ylab="log((med.ratio - max)/(min-med.ratio))", xlim = c(5.4,8.0), ylim=c(-1.5,0.6))
+  abline(pka.model)
+  abline(h=0, lty="dotted")
+  title(paste("pKa plot", id, sep = " "))
 
-  convert.to.pH <- function(FITC.value=NA, BV.value=NA, ratio.value=NA, p=params, lims=y2, ratio=F){
-    # ratio = [a / (1 + exp(-b(pH-c)))] + d
-    # pH = [log(a / (ratio - d) - 1) / -b] + c
-    if (ratio == T) corrected.ratio = ratio.value
-    else corrected.ratio <- (BV.value - BV.m.bkg) / (FITC.value - FITC.m.bkg)
-    min.ratio = min(y2)
-    max.ratio = max(y2)
-    ifelse(corrected.ratio < min.ratio, NA, ifelse(corrected.ratio < max.ratio, (log((p[1]/(corrected.ratio-p[4])) - 1)/-p[2]) + p[3], NA))
+  # Completion statement
+  cat(id, "pKa =", -pka.model[[1]]/pka.model[[2]], sep = " ")
   }
-  fxn.name <- paste("convert.to.pH", as.character(id), sep=".")
-  assign(fxn.name, convert.to.pH, envir = .GlobalEnv)
-
-  # Completion statement:
-  cat("Calibration curve with experiment ID \'", id, "\' complete:\n", sep="")
-  print(q)
-  cat(cc.quality, "% of data was retained\n", sep="")
-  cat("To convert with this calibration curve, use the function \'", fxn.name, "\'\n", sep="")
 }
